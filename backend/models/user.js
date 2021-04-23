2aca0bc95e25aab84a675916748aec8d41d6cd9a
khizrim <kh.makhmudov@me.com>
2021-04-23 22:39:22 +0300

    fix: send correct data type on login

--- backend/models/user.js
+++ backend/models/user.js
@@ -40,10 +40,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function findUserByCredentials({
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
   email,
   password,
-}) {
+) {
   return this.findOne({ email })
     .select('+password')
     .then((user) => {
