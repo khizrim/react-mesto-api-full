5e98e0ac68cf729971d79410a07548dc243fc99c
khizrim <kh.makhmudov@me.com>
2021-04-24 21:11:32 +0300

    feat: add server crash test

--- backend/app.js
+++ backend/app.js
@@ -36,6 +36,12 @@ app.use(express.json());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', validateSignInBody, login);
 app.post('/signup', validateSignUpBody, createUser);
 
